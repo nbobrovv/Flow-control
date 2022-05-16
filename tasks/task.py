5f9552e5256dece417c2594e0b98a972b3765e15
nbobrov8 <bobrovn8@yandex.ru>
2022-05-16 11:02:18 +0300

    Исправлено задание

--- tasks/task.py
+++ tasks/task.py
@@ -14,55 +14,51 @@ CONST_ABSOLUTE = 1e-07
 
 
 # Находим контрольное значение
-def control_x(x=1):
-    return math.pow(3, x)
+def control_x(x=0.7):
+    return 1 / (1 - x)
 
 
 def control_y(x=-0.8):
     return 5 - 2 * x / (6 - 5 * x + math.pow(x, 2))
 
 
-def summ_1():
-    x = 1
-    previous = 0
+def summ_1(x):
     s = 0
     n = 0
-    curr = (math.pow(x, n) * math.pow(math.log(3), n)) / math.factorial(n)
-    s += curr
-    n += 1
-    while abs(curr - previous) > CONST_ABSOLUTE:
-        previous = curr
-        curr = (math.pow(x, n) * math.pow(math.log(3), n)) / math.factorial(n)
+    curr = 0
+    while True:
+        pre = math.pow(x, n)
         n += 1
+        if abs(curr - pre) < CONST_ABSOLUTE:
+            break
+        curr = math.pow(x, n)
         s += curr
     return s
 
 
-def summ_2():
-    x = -0.8
-    previous = 0
+def summ_2(x):
     s = 0
     n = 0
-    curr = (1 / math.pow(2, n) + 1 / math.pow(3, n)) * math.pow(x, n-1)
-    s += curr
-    n += 1
-    while abs(curr - previous) > CONST_ABSOLUTE:
-        previous = curr
+    curr = 0
+    while True:
+        previous = (1 / math.pow(2, n) + 1 / math.pow(3, n)) * math.pow(x, n-1)
+        n += 1
+        if abs(curr - previous) < CONST_ABSOLUTE:
+            break
         curr = (1 / math.pow(2, n) + 1 / math.pow(3, n)) * math.pow(x, n-1)
-        n += 1
         s += curr
     return s
 
 
-def compare(x, y):
-    result = x - y
+def compare(first, second, x):
+    result = first(x) - second(x)
     print(f"Результат сравнения {result}")
 
 
 if __name__ == '__main__':
-    th1 = Thread(target=compare(summ_1(), control_x()))
+    th1 = Thread(target=compare(summ_1, control_x, 0.7))
+    th2 = Thread(target=compare(summ_2, control_y, -0.8))
     th1.start()
-    th2 = Thread(target=compare(summ_2(), control_y()))
     th2.start()
     th1.join()
     th2.join()
